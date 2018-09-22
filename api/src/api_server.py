a22387def70db63b63c6220c53ddb3cd48200361
ahmed-mez <ahmed.mezghani@outlook.com>
2018-09-22 12:29:15 +0200

    better exception handling + logging

--- api/src/api_server.py
+++ api/src/api_server.py
@@ -8,11 +8,13 @@ from utils import b64_encoding
 from redis import StrictRedis
 from io import BytesIO
 from uuid import uuid4
+import logging
 import json
 import time
 
 app = Flask(__name__)
 db = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+logging.basicConfig(filename='../api.log', level=logging.INFO)
 
 def prepare_image(image):
     """ Prepare image to be processed
@@ -59,11 +61,13 @@ def predict():
             try:
                 image = prepare_image(image)
             except Exception:
-                raise
+                logging.exception("Error in preparing image")
+                return jsonify(data), 400
             im_id = str(uuid4())
             im_dict = {"im_id": im_id, "image": b64_encoding(image)}
             # send image to the redis queue
             db.rpush(IMAGE_QUEUE, json.dumps(im_dict))
+            logging.info("New image added to queue with id: %s", im_id)
             while True:
                 # start polling
                 output = db.get(im_id)
@@ -72,13 +76,19 @@ def predict():
                     try:
                         output = output.decode("utf-8")
                         data["predictions"] = json.loads(output)
-                        db.delete(im_id)
+                        logging.info("Got predictions for image with id: %s", im_id)
                     except Exception:
-                        raise
+                        logging.exception("Error in getting predictions for image with id: %s", im_id)
+                        return jsonify(data), 400
+                    finally:
+                        db.delete(im_id)
+                        logging.info("Deleting image from queue with id: %s", im_id)
                     break
                 time.sleep(CONSUMER_SLEEP)
             data["success"] = True
+            logging.info("Send result for image with id: %s", im_id)
             return jsonify(data), 200
+    logging.warning("Invalid request with file %s", request.files)
     return jsonify(data), 400
 
 if __name__ == "__main__":
